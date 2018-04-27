90f61b66fa93c7819c8845a1bfc5dff3a09c3356
Yuri Dogandjiev <yudogan@microsoft.com>
2018-04-26 17:32:09 -0700

    Update app IDs

--- src/services/msal.auth.service.js
+++ src/services/msal.auth.service.js
@@ -7,7 +7,7 @@ class MsalAuthService {
     let redirectUri = window.location.origin;
 
     this.applicationConfig = {
-      clientID: "36b1586d-b1da-45d2-9b32-899c3757b6f8"
+      clientID: "ab93102c-869b-4d34-a921-a31d3e7f76ef"
     };
 
     this.app = new Msal.UserAgentApplication(
